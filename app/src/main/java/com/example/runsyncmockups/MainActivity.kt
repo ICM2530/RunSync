9d1f489f54fa5208da33b799789ec96b6a255ce9
sebas <sebastiansanchez3004@gmail.com>
2025-10-21 09:07:55 -0500

    conexion firebase

--- app/src/main/java/com/example/runsyncmockups/MainActivity.kt
+++ app/src/main/java/com/example/runsyncmockups/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.runsyncmockups.Navigation.Navigation
 import com.example.runsyncmockups.ui.theme.RunSyncMockUpsTheme
 
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/runsyncmockups/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/runsyncmockups/ui/ProfileScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import com.example.runsyncmockups.ui.mocks.PantallaActividad
 import com.example.runsyncmockups.ui.mocks.PantallaPerfil
 
 @Composable
