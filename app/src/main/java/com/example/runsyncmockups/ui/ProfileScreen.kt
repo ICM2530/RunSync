e71bf68f6572aa169cb44696cb198bad8da2088b
sebas <sebastiansanchez3004@gmail.com>
2025-10-21 23:25:02 -0500

    agregar boton de cerrar session

--- app/src/main/java/com/example/runsyncmockups/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/runsyncmockups/ui/ProfileScreen.kt
@@ -9,26 +9,62 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.runsyncmockups.Navigation.AppScreens
+import com.example.runsyncmockups.firebaseAuth
 //import com.example.runsyncmockups.ui.mocks.PantallaActividad
 import com.example.runsyncmockups.ui.mocks.PantallaPerfil
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(navController: NavController) {
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title= { Text("Perfil",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                )}, actions =  {
+                    IconButton(onClick = {
+                        firebaseAuth.signOut()
+                        navController.navigate(AppScreens.InicioSesion.name){
+                            popUpTo(AppScreens.Profile.name){
+                                inclusive = true
+                            }
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Logout,
+                            contentDescription = "Cerrar sesión",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+
+            )
+        },
         bottomBar = { BottomBarView(navController) }
     ) { padding ->
         Box(
