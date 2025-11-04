e135df84e13851c16e01cae3317e785b7adaaceb
sebas <sebastiansanchez3004@gmail.com>
2025-11-04 08:49:47 -0500

    Conexion con BD real time de firebase

--- app/src/main/java/com/example/runsyncmockups/MainActivity.kt
+++ app/src/main/java/com/example/runsyncmockups/MainActivity.kt
@@ -5,18 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.runsyncmockups.Navigation.Navigation
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 val firebaseAuth = FirebaseAuth.getInstance()
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+
         enableEdgeToEdge()
         setContent {
             Navigation()
         }
     }
 }
+
 
 
